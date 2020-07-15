217d27b416631972bc5b2f6b3ebcac61c6f19534
barrientoserwin <barrientoserwin381@gmail.com>
2020-07-15 04:06:25 -0400

    2da version

--- resources/js/pagina/pages/medidas.vue
+++ resources/js/pagina/pages/medidas.vue
@@ -3,43 +3,148 @@
   <div class="row main" wfd-id="51">
     <div class="card" style="width:100%;" wfd-id="52">
       <div class="card-body" wfd-id="55">
-        <h2 class="card-title">Medidas y Recomendaciones</h2>
+        <h2 class="card-title">Recomendaciones</h2>
       </div>
 
       <div class="card-body" wfd-id="54">
         <p align="center">
           <img
             class="img-fluid"
-            src="/img/pagina/sangrevale3.jpg"
+            src="/img/pagina/medidas.jpg"
             alt="Donar sangre y plasma"
-          />
-        </p>
-        <p align="center">
-          <img
-            class="img-fluid"
-            src="/img/pagina/guia-desinformacion.jpg"
-            alt="No a la desinformacion"
-          />
-        </p>
-        <p align="center">&nbsp;</p>
-        <p align="center">
-          <img
-            class="img-fluid"
-            src="/img/pagina/medidas.png"
-            alt="COVID-19"
-          />
-          <br />
-          <img
-            class="img-fluid"
-            src="/img/pagina/medidas2.png"
-            alt="BoliviaSegura"
           />
         </p>
       </div>
 
+      <div> 
+        <div class="row cards" wfd-id="44">
+          <div class="card-deck" wfd-id="45">
+            
+            <div class="card" wfd-id="52">
+              <div class="card-body" wfd-id="53">
+                <h5 class="card-title">Lavate las manos</h5>
+                <p class="card-text">
+                  <img
+                    src="/img/pagina/bio1.png"
+                    align="left"
+                  />
+                  Lávese las manos con frecuencia con agua y jabón por al menos 20 segundos, 
+                  especialmente después de haber estado en un lugar público, o después de sonarse la nariz, toser o estornudar.
+                  Si no dispone de agua y jabón, use un desinfectante de manos que contenga al menos un 60 % de alcohol. 
+                  Cubra todas las superficies de las manos y frótelas hasta que sienta que se secaron.
+                </p>
+              </div>
+            </div>
+
+            <div class="card" wfd-id="50">
+              <div class="card-body" wfd-id="51">
+                <h5 class="card-title">Utiliza barbijos</h5>
+                <p class="card-text"> 
+                  <img
+                    src="/img/pagina/bio4.png"
+                    width="128"
+                    align="left"
+                  />
+                  Usted podría propagar el COVID-19 incluso si no se siente enfermo. La cubierta de tela para la cara es para proteger a las demás personas en caso de que usted esté infectado.
+                  Todas las personas deberían usar una cubierta de tela para la cara en ámbitos públicos y cuando están cerca de personas que no viven en su casa, especialmente 
+                  cuando otras medidas de distanciamiento social son difíciles de mantener.
+                </p>
+              </div>
+            </div>
+
+            <div class="card" wfd-id="48">
+              <div class="card-body" wfd-id="49">
+                <h5 class="card-title">No tocarse la cara</h5>
+                <p class="card-text">
+                  <img
+                    src="/img/pagina/bio3.png"
+                    alt="Paciente en casa"
+                    align="left"
+                  /> 
+                  No tocarse la cara con las manos sucias, sobre todo nariz, boca y ojos
+                </p>
+              </div>
+            </div>
+
+            <div class="card" wfd-id="46">
+              <div class="card-body" wfd-id="47">
+                <h5 class="card-title">Alimientos bien cocidos</h5>
+                <p class="card-text">
+                  <img
+                    src="/img/pagina/bio7.png"
+                    align="left"
+                  /> 
+                  Solo come alimentos bien cocinados y bebe agua que haya sido desinfectada o embotellada comercialmente
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row cards" wfd-id="32">
+          <div class="card-deck" wfd-id="33">
+            <div class="card" wfd-id="42">
+              <div class="card-body" wfd-id="43">
+                <h5 class="card-title">Cubrete la boca</h5>
+                <p class="card-text">
+                <img
+                    src="/img/pagina/bio2.png"
+                    width="60"
+                    align="left"
+                  /> 
+                  Cúbrase siempre la boca y la nariz con un pañuelo desechable al toser o estornudar o cúbrase con la parte interna del codo y no escupa.
+                  Bote los pañuelos desechables usados a la basura. Lávese las manos inmediatamente con agua y jabón por al menos 20 segundos
+                </p>
+              </div>
+            </div>
+
+            <div class="card" wfd-id="40">
+              <div class="card-body" wfd-id="41">
+                <h5 class="card-title">Limpiar Superficies</h5>
+                <p class="card-text">
+                  <img
+                    src="/img/pagina/bio5.png"
+                    align="left"
+                  /> 
+                  Limpie Y desinfecte diariamente las superficies que se tocan con frecuencia . Esto incluye las mesas, las manijas de las puertas, 
+                  los interruptores de luz, los mesones, las barandas, los escritorios, los teléfonos, los teclados, los inodoros, los grifos, los lavamanos y los lavaplatos
+                </p>
+              </div>
+            </div>
+
+            <div class="card" wfd-id="38">
+              <div class="card-body" wfd-id="39">
+                <h5 class="card-title">En caso de sintomas (Quedate en casa)</h5>
+                <p class="card-text">
+                <img
+                    src="/img/pagina/bio6.png"
+                    align="left"
+                  /> 
+                  Quedarse en casa cuando se tienen enfermedades respiratorias y acudir al medico si se presenta alguno de los sintomas
+                </p>
+              </div>
+            </div>
+
+            <div class="card" wfd-id="34">
+              <div class="card-body" wfd-id="35">
+                <h5 class="card-title">Saludar a distancia</h5>
+                <p class="card-text">
+                <img
+                    src="/img/pagina/bio8.png"
+                    align="left"
+                  /> 
+                  Dentro de su casa: evite tener contacto con personas que están enfermas.
+                  De ser posible, mantenga una distancia de 6 pies entre la persona enferma y otros miembros de su hogar.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div class="card-body" wfd-id="53">
         <p class="text-right">
-          <a href="https://www.boliviasegura.gob.bo/index.php">
+          <a href="http://127.0.0.1:8000/#/inicio">
             <button type="button" class="btn btn-sm btn-success" wfd-id="87">Volver al inicio</button>
           </a>
         </p>
